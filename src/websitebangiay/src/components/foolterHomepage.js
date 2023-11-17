f0c9312de979b6bb33578849c2a6df0a36165cc4
Minurte1 <hohoangphucjob@gmail.com>
2023-11-17 12:57:33 +0700

    update fix css

--- src/websitebangiay/src/components/foolterHomepage.js
+++ src/websitebangiay/src/components/foolterHomepage.js
@@ -10,20 +10,20 @@ function Footer() {
                 <p className="logo">phuc_magin2px</p>
                 <div className="foolter">
                     <div className="footer-thongtinlienhe">Thông Tin Liên Hệ
-                        <p>Địa chỉ:   Tỉnh Trà Vinh</p>
+                        <p className="footer-thongtinlienhe_diachi">Địa chỉ:   Tỉnh Trà Vinh</p>
                         <p>Email: hohoangphucjob@gmail.com</p>
                         <p>Hotline: 037248322</p>
                     </div>
                     <div className="footer-chinhsach">Chính Sách
-                        <p>Bảo hành 12 tháng</p>
+                        <p className="footer-chinhsach_baohanh">Bảo hành 12 tháng</p>
                         <p>Đổi trả sau 3 ngày </p>
                         <p>Miễn ship cho lần đầu mua hàng</p>
                     </div>
-                    <div className="footer-hotro">Hỗ Trợ <p><a href="/">Tuyển dụng</a></p> </div>
-                    <div className="footer-mangxahoi">Mạng xã hội<p><a href="/">Facebook</a></p>
-                        <p><a href="/">Instagram</a></p>
-                        <p><a href="/">Tiktok</a></p></div>
-                </div></div>
+                    <div className="footer-hotro">Hỗ Trợ <p><a href="/" className="thea footer-hotro_tuyendung">Tuyển dụng</a></p> </div>
+                    <div className="footer-mangxahoi">Mạng xã hội<p><a href="/" className="thea footer-facebook">Facebook</a></p>
+                        <p><a href="/" className="thea">Instagram</a></p>
+                        <p><a href="/" className="thea">Tiktok</a></p></div>
+                </div></div >
         </>
 
     );
