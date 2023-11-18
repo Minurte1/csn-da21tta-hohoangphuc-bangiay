8240e027275e34cd7bd7f980cfdb67878e3a6e83
Minurte1 <hohoangphucjob@gmail.com>
2023-11-18 21:20:50 +0700

    push

--- src/websitebangiay/src/components/NavbarhomePage.js
+++ src/websitebangiay/src/components/NavbarhomePage.js
@@ -2,7 +2,7 @@
 import '../assets/styles/NavBar.css'
 import React, { useState } from 'react';
 import LogoNavbar from '../assets/image-logo/logoNavbar.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
 
 const MyNavbar = () => {
@@ -16,7 +16,10 @@ const MyNavbar = () => {
         <div className='navbar' id='navbar'>
             <div color="light" light expand="md" className='nav'>
                 <div isOpen={isOpen} className='isOpen'>
-                    <img src={LogoNavbar} alt="Logo" className='image-navbar' />
+                    {/* <img src={LogoNavbar} alt="Logo" className='image-navbar' /> */}
+                    <a className='image-navbar'>
+                        Phuc_Magin2px
+                    </a>
                     <div className="ml-auto" >
                         <a href="/" className='nav-item nav-item1'>Nữ</a>
                         <a href="/about" className='nav-item nav-item2'>Nam</a>
