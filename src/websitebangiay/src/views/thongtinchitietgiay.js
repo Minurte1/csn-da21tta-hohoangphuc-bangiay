37c8603a91e071a6222a32ffab12fd9cc476c3c5
Minurte1 <hohoangphucjob@gmail.com>
2023-11-20 21:32:21 +0700

    update thong tin chi tiet

--- src/websitebangiay/src/views/thongtinchitietgiay.js
+++ src/websitebangiay/src/views/thongtinchitietgiay.js
@@ -1,28 +1,70 @@
-// ThongTinChiTietGiay.js
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import '../assets/styles/thongtinchitietgiay.css'; // Import tệp CSS
+import '../assets/styles/thongtinchitietgiay.css';
 
 const ThongTinChiTietGiay = () => {
     const { state } = useLocation();
+    const [counterValue, setCounterValue] = useState(1);
+    const [selectedSize, setSelectedSize] = useState(null);
 
     const handleClickMuaHang = (event) => {
         event.preventDefault();
-        alert('Chúc mừng bạn đã mua hàng thành công');
-        // Thêm mã xử lý mua hàng của bạn ở đây nếu cần
+
+        if (selectedSize) {
+            alert(`Chúc mừng bạn đã mua hàng thành công - Giày ${state.name} với  Size: ${selectedSize} `);
+            // Thêm mã xử lý mua hàng của bạn ở đây nếu cần
+        } else {
+            alert('Vui lòng chọn size trước khi mua hàng.');
+        }
+    };
+
+    const handleSizeClick = (size) => {
+        setSelectedSize(size);
+    };
+
+    const increment = () => {
+        setCounterValue(counterValue + 1);
+    };
+
+    const decrement = () => {
+        if (counterValue > 1) {
+            setCounterValue(counterValue - 1);
+        }
     };
 
     return (
         <div className="container">
-
             <div className="product-info">
-                <form>
-                    <p>Product ID: {state.id}</p>
-                    <p>Name: {state.name}</p>
-                    <p>Image: <img className="product-image" src={state.image} alt={state.name} /></p>
-                    <p>Price: ${state.price}</p>
-
-                    <button className="purchase-button" onClick={handleClickMuaHang}>Mua Hàng</button>
+                <form className='form'>
+                    <div className="product-image">
+                        <img className="image" src={state.image} alt={state.name} />
+                    </div>
+                    <div className='product-h3'>
+                        <h3 className='h3'> Giày {state.name}</h3>
+                        <p className='product-price'>${state.price}</p>
+                        <div className='size'>
+                            <p className='size-p'>Size</p>
+                            {['37', '38', '39', '40'].map(size => (
+                                <div
+                                    key={size}
+                                    className={`size-option ${selectedSize === size ? 'selected' : ''}`}
+                                    onClick={() => handleSizeClick(size)}
+                                >
+                                    <p className={`size-text ${selectedSize === size ? 'selected-text' : ''}`}>{size}</p>
+                                </div>
+                            ))}
+                        </div>
+                        <hr></hr>
+                        <p className='con-hang'>Còn hàng</p>
+                        <div className='product-h3_muahang'>
+                            <div className="counter-container">
+                                <div onClick={decrement} className='span1'>-</div>
+                                <input type="text" id="counter" value={counterValue} min={1} max={5000} readOnly />
+                                <div onClick={increment} className='span2'><p>+</p></div>
+                            </div>
+                            <button className="purchase-button" onClick={handleClickMuaHang}>Mua Hàng</button>
+                        </div>
+                    </div>
                 </form>
             </div>
         </div>
